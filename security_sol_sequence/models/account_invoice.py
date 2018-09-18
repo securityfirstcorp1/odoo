6cb80db7df01d3c13a19f3e70499649dfc03fc6a
Prashant <ppa@odoo.com>
2018-09-18 09:58:09 -0700

    [IMP] renamed the field lael to item

--- security_sol_sequence/models/account_invoice.py
+++ security_sol_sequence/models/account_invoice.py
@@ -7,4 +7,4 @@ from odoo import fields, models
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
-    seq_line_no = fields.Char('Sequence', help="10 and 20 , etc.....")
+    seq_line_no = fields.Char('Item', help="10 and 20 , etc.....")
